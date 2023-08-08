750d1df67943f559e711f0d728c0e96ca078bb01
Thaliaraujo <thalia.araujo321@gmail.com>
2023-08-08 18:21:17 -0300

    commit

--- assets/js/connectAPI.js
+++ assets/js/connectAPI.js
@@ -1,9 +1,28 @@
 async function listVideos() {
-    const connection = await fetch('http://localhost:3000/videos');
+    const connection = await fetch("http://localhost:3000/videos");
     const convertedConnection = await connection.json();
     return convertedConnection;
 };
 
+async function createVideo(title, description, url, image) {
+    const connect = await fetch ("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            description: `${description} mil visualizações`,
+            url: url,
+            image: image
+        })
+    });
+
+    const convertedConnection = await connect.json();
+    return convertedConnection;
+};
+
 export const connectAPI = {
-    listVideos
+    listVideos, 
+    createVideo
 };
