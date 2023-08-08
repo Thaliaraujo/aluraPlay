d9564a93f917db5aad174f4241b79bb9c8e490bf
Thaliaraujo <thalia.araujo321@gmail.com>
2023-08-08 18:57:37 -0300

    commit

--- assets/js/createVideo.js
+++ assets/js/createVideo.js
@@ -1,12 +1,18 @@
+import { connectAPI } from "./connectAPI.js";
+
 const form = document.querySelector("[data-form]");
 
-function createVideo(evento) {
+async function createVideo(evento) {
     evento.preventDefault();
 
     const image = document.querySelector("[data-image]").value;
     const title = document.querySelector("[data-title]").value;
     const url = document.querySelector("[data-url]").value;
     const description = Math.floor(Math.random() * 10).toString();
+
+    await connectAPI.createVideo(title, description, url, image);
+
+    window.location.href = "../pages/videoSent.html";
 };
 
 form.addEventListener("submit", evento => createVideo(evento));
